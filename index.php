76db7a3038bd92c5c208e8d80194c9d733134590
unknown <phamhoangvjp123@gmail.com>
2024-03-11 23:26:55 +0700

    add file

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<form action="welcome.php" method="POST">
+Name: <input type="text" name="name"><br>
+Username: <input type="text" name="user"><br>
+
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,11 @@
+<?php 
+echo $_POST["name"];
+
+echo " <h1>Username vừa nhập </h1> <br>" ;
+
+echo "my name is " . $_POST["user"] . " haha s <br>";
+
+
+echo $_POST["email"];
+
+?>
