04e8dc91e012257faf9b865f35456da0f27d3e18
unknown <phamhoangvjp123@gmail.com>
2024-03-13 12:46:11 +0700

    add ajax

---
+++ TESTAJAX/index.php
@@ -0,0 +1,33 @@
+<html>
+
+<head>
+    <script>
+        function showHint(str) {
+            if (str.length == 0) {
+                document.getElementById("txtHint").innerHTML = "";
+                return;
+            } else {
+                var xmlhttp = new XMLHttpRequest();
+                xmlhttp.onreadystatechange = function () {
+                    if (this.readyState == 4 && this.status == 200) {
+                        document.getElementById("txtHint").innerHTML = this.responseText;
+                    }
+                };
+                xmlhttp.open("GET", "gethint.php?q=" + str, true);
+                xmlhttp.send();
+            }
+        }
+    </script>
+</head>
+
+<body>
+
+    <p><b>Start typing a name in the input field below:</b></p>
+    <form action="">
+        <label for="fname">First name:</label>
+        <input type="text" id="fname" name="fname" onkeyup="showHint(this.value)">
+    </form>
+    <h1>Suggestions: <span id="txtHint"></span></h1>
+</body>
+
+</html>
